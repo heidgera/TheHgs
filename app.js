d0dd15d0a6d501c14b2b28e8580cdc61ed684f27
ajhg <heidgera@gmail.com>
2017-09-27 21:44:44 -0500

    changed client checker

--- app.js
+++ app.js
@@ -13,14 +13,22 @@ obtain(obtains, ({ fileServer }, { wss }, { exec }, { devices })=> {
 
   //var server = PeerServer({ port: 9000, path: '/freebook' });
 
+  var AaronHomeState = false;
+
   setInterval(()=> {
     exec('sudo hostapd_cli all_sta', (err, stdout, stderr)=> {
       if (stdout.includes('64:bc:0c:4b:da:81')) {
-        console.log('Aaron is home');
-        devices.sunroomLamp.turnOn();
+        if (!AaronHomeState) {
+          console.log('Aaron is home');
+          devices.sunroomLamp.turnOn();
+          AaronHomeState = true;
+        }
       } else {
-        console.log('Aaron is away');
-        devices.sunroomLamp.turnOff();
+        if (AaronHomeState) {
+          console.log('Aaron is away');
+          devices.sunroomLamp.turnOff();
+          AaronHomeState = false;
+        }
       }
     });
   }, 5000);
