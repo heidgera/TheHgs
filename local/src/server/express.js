a283a31aced3bd3b5f437c2b7abae84d8374734f
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-01-29 13:05:12 -0600

    changed to julia

--- local/src/server/express.js
+++ local/src/server/express.js
@@ -9,13 +9,11 @@ obtain(['express', 'body-parser', 'fs'], (express, bodyParser, fs)=> {
   fileServer.use('/common', express.static('./common'));
 
   fileServer.start = ()=> {
-
+    fileServer.use(router);
+    fileServer.listen(80, function () {
+      console.log('listening on 80');
+    });
   };
-
-  fileServer.use(router);
-  fileServer.listen(80, function () {
-    console.log('listening on 80');
-  });
 
   exports.fileServer = fileServer;
   exports.router = router;
